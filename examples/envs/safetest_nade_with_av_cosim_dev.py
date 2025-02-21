fe1db1adaffe90585681bb74d1449c874ae20a4d
Haowei Sun <printfplus@outlook.com>
2025-02-21 14:39:35 -0500

    Attempted: map and route files added for pedestrian co-simulation

--- examples/envs/safetest_nade_with_av_cosim_dev.py
+++ examples/envs/safetest_nade_with_av_cosim_dev.py
@@ -4,13 +4,294 @@ import random
 import terasim.utils as utils
 from terasim_nde_nade.envs.safetest_nade_with_av import SafeTestNADEWithAV
 from sumolib.net import Net, readNet
-from terasim_cosim.terasim_plugin.utils.VRU import (
-    ExternalPerson,
-    JayWalkingPerson,
-    StopOnCrossingPerson,
-    RunRedLightPerson,
-    AlteratingRidingBike,
-)
+from terasim.overlay import traci
+import math
+
+from sumolib.net import *
+from sumolib.net.edge import *
+from sumolib.net.lane import *
+from sumolib.net.connection import *
+from abc import ABC, abstractmethod
+
+
+class ExternalPerson(ABC):
+
+    def __init__(self, id):
+        self.id: str = id
+        self.current_step: int = 0
+
+    @staticmethod
+    @abstractmethod
+    def satisfy() -> bool:
+        pass
+
+    def action(self) -> int:
+        if self.current_step == 0:
+            self.on_begin()
+        if self.until():
+            self.on_end()
+            return 1
+        else:
+            self.execute()
+            self.current_step += 1
+            return 0
+
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+    @abstractmethod
+    def until(self) -> bool:
+        pass
+
+    @abstractmethod
+    def on_begin(self) -> None:
+        pass
+
+    @abstractmethod
+    def on_end(self) -> None:
+        pass
+
+
+class JayWalkingPerson(ExternalPerson):
+
+    def __init__(self, id, sumonet: Net):
+        super().__init__(id)
+        self.angle: float = None
+        self.sumonet: Net = sumonet
+        self.edge: str = traci.person.getRoadID(id)
+        self.total_width: float = sum([lane.getWidth() for lane in sumonet.getEdge(self.edge).getLanes()])
+        self.curr_distance: float = 0
+
+    @staticmethod
+    def satisfy(p_id):
+        """
+        条件：行人走在一条有车行道的路的人行道上，人行道在最右侧，中间没有任何隔断
+        而且人离边的起终点都有一定距离
+        """
+
+        edge_id = traci.person.getRoadID(p_id)
+        lane_id = traci.person.getLaneID(p_id)
+        lane_position = traci.person.getLanePosition(p_id)
+        lane_length = traci.lane.getLength(lane_id)
+        if lane_position < 3 or lane_position > lane_length - 3:
+            return False
+
+        num_lanes: int = traci.edge.getLaneNumber(edge_id)
+        has_sidewalk: bool = False
+        has_road: bool = False
+        have_separation: bool = False
+
+        for i in range(num_lanes):
+            lane_id = f"{edge_id}_{i}"
+            allowed: list[str] = traci.lane.getAllowed(lane_id)
+            disallowed: list[str] = traci.lane.getDisallowed(lane_id)
+            if i == 0 and "pedestrian" in allowed:
+                has_sidewalk = True
+            if (
+                "passenger" in allowed
+                or "private" in allowed
+                or "pedestrian" in disallowed
+            ):
+                has_road = True
+            if i > 0 and i < num_lanes - 1 and "all" in disallowed:
+                return False
+
+        return has_sidewalk and has_road and (not have_separation)
+
+    def on_begin(self):
+        """让行人转一个方向，如果正向行走，就左转90度；否则右转90度"""
+        print("call")
+
+        def is_pedestrian_moving_forward(p_id):
+
+            edge_id = traci.person.getRoadID(p_id)
+            edge_shape = self.sumonet.getEdge(edge_id).getShape()
+            edge_start = edge_shape[0]
+            edge_end = edge_shape[-1]
+            edge_angle = (
+                -math.degrees(
+                    math.atan(
+                        (edge_end[0] - edge_start[0]) / (edge_end[1] - edge_start[1])
+                    )
+                )
+                + 90
+            ) % 360
+            person_angle = traci.person.getAngle(self.id)
+            print((edge_angle - person_angle) % 360)
+            if (edge_angle - person_angle) % 360 < 180:
+                return True
+            else:
+                return False
+
+        current_angle = traci.person.getAngle(self.id)
+        if is_pedestrian_moving_forward(self.id):
+            self.angle = (current_angle - 90) % 360  # 左转
+            print("turn left")
+        # else:
+        #     self.angle = (current_angle + 90) % 360  # 右转
+        #     print("turn right")
+
+    def execute(self):
+        self.curr_distance += force_person_move_forward(self.id, mode=0b110, angle=self.angle)
+
+    def until(self):
+        """当行人走出一开始的边时，结束"""
+        return self.curr_distance >= self.total_width
+        # return self.current_step == 10
+
+    def on_end(self):
+        """结束时，直接删掉这个行人"""
+        traci.person.remove(self.id)
+
+
+class StopOnCrossingPerson(ExternalPerson):
+
+    def __init__(self, id):
+        super().__init__(id)
+        self.total_time: int = max(int(random.gauss(10, 3)), 0)
+
+    @staticmethod
+    def satisfy(p_id, sumonet: Net) -> bool:
+        edge_id = traci.person.getRoadID(p_id)
+        return sumonet.getEdge(edge_id).getFunction() == "crossing"
+
+    def on_begin(self) -> None:
+        pass
+
+    def execute(self):
+        traci.person.setSpeed(self.id, 0)
+
+    def until(self) -> bool:
+        return traci.simulation.getDeltaT() * self.current_step >= self.total_time
+
+    def on_end(self) -> None:
+        traci.person.setSpeed(self.id, traci.person.getMaxSpeed(self.id))
+
+
+class AlteratingRidingBike(ExternalPerson):
+
+    def __init__(self, id) -> None:
+        super().__init__(id)
+        self.turn_angle = 15
+        self.curr_angle = None
+        self.step_number = None  
+
+    @staticmethod
+    def satisfy(p_id, sumonet: Net) -> bool:
+        """
+        1在车行道上行驶
+        2不能太靠近当前边的终点"""
+        
+        edge_id = traci.person.getRoadID(p_id)
+        is_edge_special = sumonet.getEdge(edge_id).isSpecial()
+        if is_edge_special:
+            return False
+        
+        lane_id = traci.person.getLaneID(p_id)
+        allowed: list[str] = traci.lane.getAllowed(lane_id)
+        if "bicycle" not in allowed:
+            return False
+        if allowed == ["bicycle"]:
+            return False
+        
+        lane_length = traci.lane.getLength(lane_id)
+        lane_position = traci.person.getLanePosition(p_id)
+        if (lane_length - lane_position) < 60:
+            return False
+        
+        return True
+    
+    def on_begin(self) -> None:
+        lane_id = traci.person.getLaneID(self.id)
+        lane_width = traci.lane.getWidth(lane_id)
+        theta = math.radians(self.turn_angle)
+        distance = lane_width / math.sin(theta)
+        self.step_number = math.floor( distance / traci.person.getMaxSpeed(self.id) / traci.simulation.getDeltaT() )
+
+    def execute(self) -> None:
+        """开始时：让它左偏一定度数，然后直走；直走固定距离后，往右偏，再走固定步数；最后角度回归"""
+        if self.current_step == 0:
+            self.curr_angle = (traci.person.getAngle(self.id) - self.turn_angle) % 360
+        if self.current_step == self.step_number:
+            self.curr_angle = (traci.person.getAngle(self.id) + 2 * self.turn_angle) % 360
+        elif self.current_step == 2 * self.step_number - 1:
+            self.curr_angle = (traci.person.getAngle(self.id) + self.turn_angle) % 360
+
+        force_person_move_forward(self.id, angle=self.curr_angle, mode=0b110)
+            
+    
+    def until(self) -> bool:
+        return self.current_step == 2 * self.step_number
+    
+    def on_end(self) -> None:
+        pass
+        
+
+class RunRedLightPerson(ExternalPerson):
+
+    def __init__(self, id):
+        super().__init__(id)
+        self.lane = traci.person.getLaneID(self.id)
+
+    @staticmethod
+    def satisfy(p_id) -> bool:
+        """
+        行人必须在一条被红绿灯控制的lane上，并且它的速度很小，我们就判断它在等红灯
+        """
+        # should be waiting
+        if traci.person.getSpeed(p_id) > 0.2:
+            return False
+
+        lane_id = traci.person.getLaneID(p_id)
+        tls_list = traci.trafficlight.getIDList()
+        for tls_id in tls_list:
+            controlled_lanes = traci.trafficlight.getControlledLanes(tls_id)
+            if lane_id in controlled_lanes:
+                if traci.person.getVehicleClass(p_id) == "pedestrian":
+                    return True
+                elif traci.person.getVehicleClass(p_id) == "bicycle":
+                    if traci.lane.getLength(lane_id) - traci.person.getLanePosition(p_id) < 3:
+                        return True
+                
+        return False
+    
+    def on_begin(self):
+        pass
+
+    def execute(self):
+        force_person_move_forward(self.id, mode=0b011)
+
+    def until(self) -> bool:
+        return traci.person.getLaneID(self.id) != self.lane
+    
+    def on_end(self) -> None:
+        pass
+    
+
+
+
+def force_person_move_forward(id, mode=0b110, speed=None, angle=None) -> float:
+    """强制行人向特定方向以特定速度移动
+    如果方向不给出，默认为当前方向
+    如果速度不给出，默认为该行人的最大速度
+    """
+
+    current_pos = traci.person.getPosition(id)
+    if speed == None:
+        speed = traci.person.getMaxSpeed(id)
+    distance = speed * (traci.simulation.getDeltaT())
+    if angle == None:
+        angle = traci.person.getAngle(id)
+    radians = math.radians(angle)
+    new_pos = (
+        current_pos[0] + distance * math.sin(radians),
+        current_pos[1] + distance * math.cos(radians),
+    )
+
+    traci.person.moveToXY(id,edgeID="",x=new_pos[0],y=new_pos[1],angle=angle,keepRoute=mode,matchThreshold=20)
+
+    return math.dist(current_pos,new_pos)
 
 
 class SafeTestNADEWithAVCosim(SafeTestNADEWithAV):
@@ -121,52 +402,6 @@ class SafeTestNADEWithAVCosim(SafeTestNADEWithAV):
 
     def reroute_vehicle(self, closed_lanes):
 
-        def handle_spawned_vehicle(vehicle_id, closed_lanes, sumo_net):
-            """
-            Check if a vehicle is spawned at a closed lane and move it to the first open lane on its original route.
-
-            :param vehicle_id: ID of the vehicle to check.
-            :param closed_lanes: List of closed lane IDs.
-            :param sumo_net: SUMO network object.
-            """
-
-            def is_closed_lane(lane_id):
-                return lane_id in closed_lanes
-
-            def find_first_open_lane_on_route(route):
-                """
-                Find the first open lane along the vehicle's original route.
-
-                :param route: The original route of the vehicle (list of edge IDs).
-                :return: The first open lane ID along the route, or None if no open lane is found.
-                """
-                for edge_id in route:
-                    edge = sumo_net.getEdge(edge_id)
-                    for lane in edge.getLanes():
-                        if not is_closed_lane(lane.getID()):
-                            return lane.getID()
-                return None
-
-            print("handle spawned vehicle")
-            current_lane = traci.vehicle.getLaneID(vehicle_id)
-            original_route = traci.vehicle.getRoute(vehicle_id)
-
-            if is_closed_lane(current_lane):
-                nearby_open_lane = find_first_open_lane_on_route(original_route)
-
-                if nearby_open_lane:
-                    print(
-                        f"Vehicle {vehicle_id} spawned at closed lane {current_lane}. Moving to first open lane {nearby_open_lane} on original route."
-                    )
-                    # Move the vehicle to the nearby open lane
-                    traci.vehicle.moveToLane(vehicle_id, nearby_open_lane)
-                else:
-                    print(
-                        f"Vehicle {vehicle_id} spawned at closed lane {current_lane}, but no open lane found on its original route."
-                    )
-            else:
-                print(f"Vehicle {vehicle_id} spawned at open lane {current_lane}.")
-
         print("entered reroute_vehicle")
         vehicle_ids = traci.vehicle.getIDList()
         for vehicle_id in vehicle_ids:
@@ -275,5 +510,6 @@ class SafeTestNADEWithAVCosim(SafeTestNADEWithAV):
         self.reroute_vehicle(self.closed_lane_ids)
         self.close_lane(self.closed_lane_ids)
         print("lanes closed")
+        self.vru_control()
 
         return super().on_step(ctx)
